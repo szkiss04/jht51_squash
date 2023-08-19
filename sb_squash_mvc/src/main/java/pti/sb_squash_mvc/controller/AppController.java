7c35d4262b031f36a011191fd5f5bba92a707ba3
szkiss04 <104894648+szkiss04@users.noreply.github.com>
2023-08-19 22:32:08 +0200

    Index.html and filtering by player is almost done
    It is waiting for the corresponding DAO implementation and thorough testing.

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
@@ -1,8 +1,48 @@
 package pti.sb_squash_mvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pti.sb_squash_mvc.model.Match;
+import pti.sb_squash_mvc.model.Place;
+import pti.sb_squash_mvc.model.Player;
+import pti.sb_squash_mvc.service.AppService;
 
 @Controller
 public class AppController {
-
+	
+	private final AppService service;
+	
+	@Autowired
+	public AppController(AppService service) {
+		
+		this.service = service;
+	}
+	
+	@GetMapping("/filter/player")
+	public String showFilteredResultsByPlayer(
+				@RequestParam(name = "playerEmail") String playerEmail,
+				Authentication userLoggedIn,
+				Model model
+			) {
+		
+		List<Player> playersList = service.getAllPlayers();
+		List<Place> placesList = service.getAllPlaces();
+		List<Match> matchesList = service.getAllMatchesByPlayerEmailDateDescending();
+		
+		model.addAttribute(userLoggedIn);
+		model.addAttribute(playersList);
+		model.addAttribute(placesList);
+		model.addAttribute(matchesList);
+		
+		return "index";
+	}
+	
 }
--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/service/AppService.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/service/AppService.java
@@ -1,8 +1,47 @@
 package pti.sb_squash_mvc.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import pti.sb_squash_mvc.model.Match;
+import pti.sb_squash_mvc.model.Place;
+import pti.sb_squash_mvc.model.Player;
 
 @Service
 public class AppService {
+
+	public List<Player> getAllPlayers() {
+		
+		/*
+		 * List<Player> playersList = database.getAllPlayers();
+		 * return playersList;
+		 * 
+		 */
+		
+		return null;
+	}
+
+	public List<Place> getAllPlaces() {
+		
+		/*
+		 * List<Place> placesList = database.getAllPlaces();
+		 * return placesList;
+		 * 
+		 */
+		
+		return null;
+	}
+
+	public List<Match> getAllMatchesByPlayerEmailDateDescending() {
+		
+		/*
+		 * List<Match> filteredMatches = database.getAllMatchesFilteredByUserEmailDateDesc();
+		 * return filteredMatches;
+		 * 
+		 */
+		
+		return null;
+	}
 
 }
